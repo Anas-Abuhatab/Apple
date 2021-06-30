3e48155f4809f82143206e1f5f4e4874c0cad388
ANASABU4 <>
2021-06-30 15:29:05 +0000

    add js

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Hi")
+var x= "     "
+var firstName =prompt("what is your name   ");
+var youriphone = prompt("    What is your iphone version");
+alert("Wellcome  "+ firstName )
+
+if (youriphone >= 5){
+
+document.write("<div>"+"<h3>"+ "Who can we help you" + "</h3>"+"<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjg9e0Catb89J5lz6qcpVGmISSa-3ITiJKaA&usqp=CAU alt'='logo'>"+"</div>")
+}
+else {
+  alert("You Need to upgrade your iphone ")
+}
